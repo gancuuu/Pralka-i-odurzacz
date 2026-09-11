d80d4a09391a389ecfb341e3e9ea7c20ee228d66
gancuuu <kamilganc2009@gmail.com>
2026-09-11 10:59:59 +0200

    Dodanie Klas i zrobienie wyswietlanie komunikatu

---
+++ app/src/main/java/com/onaar/inf_04_2025_01_02/Device.java
@@ -0,0 +1,8 @@
+package com.onaar.inf_04_2025_01_02;
+
+public class Device {
+
+    public void wyswietlWiadomosc(String message) {
+        System.out.println(message);
+    }
+}
---
+++ app/src/main/java/com/onaar/inf_04_2025_01_02/VacuumCleaner.java
@@ -0,0 +1,5 @@
+package com.onaar.inf_04_2025_01_02;
+
+public class VacuumCleaner extends Device {
+
+}
