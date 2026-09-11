1df02d28a558ea82cd2708baa1b05de269776891
gancuuu <kamilganc2009@gmail.com>
2026-09-11 11:01:30 +0200

    Klasa Odkurzacz zrobione

--- app/src/main/java/com/onaar/inf_04_2025_01_02/VacuumCleaner.java
+++ app/src/main/java/com/onaar/inf_04_2025_01_02/VacuumCleaner.java
@@ -2,4 +2,23 @@ package com.onaar.inf_04_2025_01_02;
 
 public class VacuumCleaner extends Device {
 
+    private boolean stan;
+
+    public VacuumCleaner() {
+        stan = false;
+    }
+
+    public void on() {
+        if (!stan) {
+            stan = true;
+            wyswietlWiadomosc("Odkurzacz włączono");
+        }
+    }
+
+    public void off() {
+        if (stan) {
+            stan = false;
+            wyswietlWiadomosc("Odkurzacz wyłączono");
+        }
+    }
 }
