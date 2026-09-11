eca6024942a357377c00660e60b6cab3f5078403
gancuuu <kamilganc2009@gmail.com>
2026-09-11 11:00:41 +0200

    Pralka Klasa zrobiona

--- app/src/main/java/com/onaar/inf_04_2025_01_02/WashingMachine.java
+++ app/src/main/java/com/onaar/inf_04_2025_01_02/WashingMachine.java
@@ -2,4 +2,19 @@ package com.onaar.inf_04_2025_01_02;
 
 public class WashingMachine extends Device {
 
+    private int nrProgram;
+
+    public WashingMachine() {
+        nrProgram = 0;
+    }
+
+    public int setWashingProgram(int program) {
+        if (program >= 1 && program <= 12) {
+            nrProgram = program;
+        } else {
+            nrProgram = 0;
+        }
+
+        return nrProgram;
+    }
 }
